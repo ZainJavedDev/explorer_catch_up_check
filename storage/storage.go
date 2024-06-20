5e98dbd579b876b449c51bad473a76d4a868447b
ZainJavedDev <zjaved.bese17seecs@seecs.edu.pk>
2024-06-20 12:08:25 +0500

    fix: change directory to new volume and build

--- storage/storage.go
+++ storage/storage.go
@@ -9,7 +9,7 @@ import (
 
 func CheckStorage() error {
 	fs := syscall.Statfs_t{}
-	err := syscall.Statfs("/mnt/volume_sgp1_01", &fs)
+	err := syscall.Statfs("/mnt/smaller_volume", &fs)
 	if err != nil {
 		fmt.Println(err)
 		return err
